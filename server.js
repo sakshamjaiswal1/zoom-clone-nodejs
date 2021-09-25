65216b573935d99f8047ea7413122fe480d59d56
Saksham Jaiswal <sjais973@gmail.com>
2021-09-25 15:56:58 +0530

    uuid

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const server= require('http').Server(app)
+const {v4:uuidv4}= require('uuid')
+app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
-    res.status(200).send('hello to zoom clone')
+    res.render('room')
 })
 
 
